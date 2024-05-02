1d3f7572169d365d727218f5cc606f368214ee28
Ishrat Amin <ishrat.trisha14@gmail.com>
2024-05-01 23:37:47 -0400

    Second commit

--- public/javascripts/home-books.js
+++ public/javascripts/home-books.js
@@ -16,3 +16,26 @@ let swiperHome = new Swiper(".home__swiper", {
       },
     },
   });
+
+  /*=============== NEW SWIPER ===============*/
+let swiperNew = new Swiper(".new__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: "auto",
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 350 viewport height, add the
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
